1095232a6cdefa964cec6a6eaed5014bdf02d47f
GarrettGularson <31548884+GarrettGularson@users.noreply.github.com>
2020-01-12 16:37:15 -0500

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,17 @@
+import javafx.stage.Stage;
+
+public class Main {
+
+	public static void main(String[] args){
+		
+		Menu thing = new Menu();
+		Stage stage = new Stage();
+		try {
+			thing.start(stage);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		
+	}
+	
+}
---
+++ Menu.java
@@ -0,0 +1,20 @@
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+ 
+public class Menu extends Application{
+
+	public void start(Stage primaryStage) throws Exception {
+		
+		Stage window = primaryStage;
+		FXMLLoader loader = new FXMLLoader();
+		loader.setLocation(getClass().getResource("/GUI Stuff.fxml"));
+		Parent content = loader.load();
+		window.setScene(new Scene(content));
+		window.show();
+		
+	}
+    
+}
